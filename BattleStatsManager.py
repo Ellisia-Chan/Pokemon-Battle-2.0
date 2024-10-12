853e7c91716a2525b7352c3ff2684ece1e0dfdd3
Ellisia <152279955+Ellisia-Chan@users.noreply.github.com>
2024-10-12 14:49:34 +0800

    Added Stats Adjustment and Program Exit control
    Added stats adjustment and a fatigue adjustment(under development) and an exit control if all selected pokemon is selected

--- BattleStatsManager.py
+++ BattleStatsManager.py
@@ -8,13 +8,16 @@ class BattleStatsManager:
         
         self.stats_table1 = PrettyTable()
         self.stats_table2 = PrettyTable()
+        
+    def SetValueToStatsTable(self):
+        pass
            
     @property
     def GetPlayer1_win_count(self) -> int:
         return self.player1_wins
     
     @GetPlayer1_win_count.setter
-    def SetPlayer1_win_count(self, value):
+    def GetPlayer1_win_count(self, value):
         if value < 0:
             raise ValueError("Value must not be negative")
         self.player1_wins += value
@@ -24,7 +27,18 @@ class BattleStatsManager:
         return self.player2_wins
     
     @GetPlayer2_win_count.setter
-    def SetPlayer2_win_count(self, value):
+    def GetPlayer2_win_count(self, value):
         if value < 0:
             raise ValueError("Value must not be negative")
         self.player2_wins += value
+        
+    @property
+    def GetTie_count(self) -> int:
+        return self.ties
+    
+    @GetTie_count.setter
+    def GetTie_count(self, value):
+        if value < 0:
+            raise ValueError("Value must not be negative")
+        self.ties += value
+    
